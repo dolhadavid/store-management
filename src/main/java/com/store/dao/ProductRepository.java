665f8d4bd8d9721fcaa2ae75f65d8e46a44fa645
David Dolha <David.Dolha@ibm.com>
2022-06-29 11:39:00 +0300

    [GENERIC] Remove unused

--- src/main/java/com/store/dao/ProductRepository.java
+++ src/main/java/com/store/dao/ProductRepository.java
@@ -3,7 +3,6 @@ package com.store.dao;
 import com.store.dto.ProductDto;
 import com.store.entity.Product;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
