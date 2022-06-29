bf9f61d111f29018d294aced4f12dd06c3539ee1
David Dolha <David.Dolha@ibm.com>
2022-06-29 11:41:05 +0300

    [GENERIC] Remove unused

--- src/main/java/com/store/controller/ProductApi.java
+++ src/main/java/com/store/controller/ProductApi.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/products")
 @Tag(name = "Product API", description = "Api used to handle products")
